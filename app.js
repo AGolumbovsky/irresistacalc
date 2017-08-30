b99a31ac1258806541a31313135c668d46dbdc09
agolumbovsky <agolumbovsky@gmail.com>
2017-08-29 20:04:27 -0400

    making feeble progress

--- app.js
+++ app.js
@@ -1,33 +1,31 @@
 
+
+
 function calc () {
 
-    var uno     =  parseInt(document.getElementById("app1")) || 0;
-    var dos     =  parseInt(document.getElementById("app2")) || 0;
-    var tres    =  parseInt(document.getElementById("app3")) || 0;
-    var four    =  parseInt(document.getElementById("app4")) || 0;
+    let uno     = Number(document.getElementById("app1").value);
+    let dos     = Number(document.getElementById("app2").value);
+    let tres    = Number(document.getElementById("app3").value);
+    let four    = Number(document.getElementById("app4").value);
 
-    console.log(typeof(dos));
+    console.log("uno is", uno, "dos is", dos);
 
     let typeConn = document.getElementById("conn-type").value;
 
     switch (typeConn) {
 
         case "series":
-            let seriesRes = uno + dos + tres + four; 
-            console.log(uno);
+            let seriesRes = uno + dos + tres + four;
             console.log("series, yo", seriesRes);
             return seriesRes;
 
         case "parallel":
-            let parallelRes = uno * dos * tres * four;
+            let parallelRes = 1 / ((1/uno) + (1/dos) + (1/tres) + (1/four));
             console.log("parallel, yo", parallelRes);
             return parallelRes;
 
         default:
             console.log("something else is there");
     }
-
-
-    alert (uno);
 
 };
