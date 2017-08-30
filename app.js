3c8acdf6dd9e6deb05b25bcd0a1f94b757e5bd5d
agolumbovsky <agolumbovsky@gmail.com>
2017-08-29 21:01:19 -0400

    the basics working

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 
-
-
 function calc () {
 
     let uno     = Number(document.getElementById("app1").value);
     let dos     = Number(document.getElementById("app2").value);
     let tres    = Number(document.getElementById("app3").value);
     let four    = Number(document.getElementById("app4").value);
-
-    console.log("uno is", uno, "dos is", dos);
 
     let typeConn = document.getElementById("conn-type").value;
 
@@ -20,6 +16,14 @@ function calc () {
             return seriesRes;
 
         case "parallel":
+            if (uno == 0 && dos == 0 && tres == 0 && four == 0) break;
+
+            if (uno <= 0)   uno   =   1;
+            if (dos <= 0)   dos   =   1;
+            if (tres <= 0)  tres  =   1;
+            if (four <= 0)  four  =   1; 
+
+
             let parallelRes = 1 / ((1/uno) + (1/dos) + (1/tres) + (1/four));
             console.log("parallel, yo", parallelRes);
             return parallelRes;
