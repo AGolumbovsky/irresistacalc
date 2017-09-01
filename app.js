6d300a416248db1bfcc2bcdc62f3f2725dfa7505
agolumbovsky <agolumbovsky@gmail.com>
2017-09-01 08:58:25 -0400

    just some comments

--- app.js
+++ app.js
@@ -14,23 +14,33 @@ function calc () {
 
         case "series":
 
+            // do the math for series connection
             let seriesRes = uno + dos + tres + four;
             console.log("series, yo", seriesRes);
+
+            // display in the <div id="result">
             resultDiv.innerHTML = seriesRes;
+
             break;
 
         case "parallel":
 
+            // break if all the fields are zero, no calc done
             if (uno == 0 && dos == 0 && tres == 0 && four == 0) break;
 
+            // to avoid division by 0
             if (uno <= 0)   uno   =   1;
             if (dos <= 0)   dos   =   1;
             if (tres <= 0)  tres  =   1;
             if (four <= 0)  four  =   1; 
 
+            // do the math for parralel connection
             let parallelRes = 1 / ((1/uno) + (1/dos) + (1/tres) + (1/four));
             console.log("parallel, yo", parallelRes);
+
+            // display in the <div id="result">
             resultDiv.innerHTML = parallelRes;
+            
             break;
 
         default:
