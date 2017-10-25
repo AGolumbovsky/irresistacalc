08345010d78a0aab6ee5540671d397b07badcdbf
agolumbovsky <agolumbovsky@gmail.com>
2017-10-25 18:44:48 -0400

    added timestamp in server console

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 8787;
 // just using dirname for now, it works
 app.use(express.static(__dirname));
 
+// let timestamp = syst
+let timestamp = new Date().toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
+
 app.listen(PORT, function() {
-    console.log("irresistacalc is live on port", PORT);
+    console.log("irresistacalc is live on port", PORT, '\n', timestamp);
 });
